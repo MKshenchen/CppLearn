1093c0a54a2243779d3ad440b7cf085784711edd
MKshenchen <215096425@qq.com>
2024-04-02 22:23:21 +0800

    add lifetime

---
+++ 编码细节/temp_obj_lifetime.cpp
@@ -0,0 +1,26 @@
+// 关于临时变量及悬垂引用:
+// return 语句中绑定到函数返回值的临时量
+// 在函数调用中绑定函数形参的临时量
+// 都将在该表达式/返回表达式被立即销毁
+#include <iostream>
+using namespace std;
+
+const string& f(const string& s) {
+    return s;
+}
+
+int main() {
+    // 以下代码等同于:
+    // auto& res = f(string("?"));
+    // 即调用string的构造函数将
+    // "?" 这个 const char[2] 字面量
+    // 转换成一个string
+    // 然后在f()的调用中被返回
+    // 直至此句代码执行结束
+    // "?" 被释放结束生存周期
+    // res 成为悬垂引用
+    auto& res = f("?");
+    cout << res << endl;
+    cin.get();
+    return 0;
+}
