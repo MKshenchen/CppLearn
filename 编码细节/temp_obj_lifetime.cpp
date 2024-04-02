aa7f1b85def2a1c847c11d772a2cb83319fddc94
MKshenchen <215096425@qq.com>
2024-04-02 22:27:58 +0800

    fix lifetime

--- 编码细节/temp_obj_lifetime.cpp
+++ 编码细节/temp_obj_lifetime.cpp
@@ -1,7 +1,9 @@
 // 关于临时变量及悬垂引用:
 // return 语句中绑定到函数返回值的临时量
-// 在函数调用中绑定函数形参的临时量
-// 都将在该表达式/返回表达式被立即销毁
+// 和在函数调用中绑定函数形参的临时量
+// 都将在该表达式/返回表达式j结束后被立即销毁
+// 如果这俩个表达式赋值给了另外的引用变量
+// 那么则为悬垂引用,是不安全的
 #include <iostream>
 using namespace std;
 
@@ -10,7 +12,8 @@ const string& f(const string& s) {
 }
 
 int main() {
-    // 以下代码等同于:
+    auto& res = f("?");
+    // 以上代码等同于:
     // auto& res = f(string("?"));
     // 即调用string的构造函数将
     // "?" 这个 const char[2] 字面量
@@ -19,7 +22,6 @@ int main() {
     // 直至此句代码执行结束
     // "?" 被释放结束生存周期
     // res 成为悬垂引用
-    auto& res = f("?");
     cout << res << endl;
     cin.get();
     return 0;
